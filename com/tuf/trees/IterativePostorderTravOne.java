5757094d594441d9bb07a699a909edca272a12b0
kiranpoojary <36165693+kiranpoojary@users.noreply.github.com>
2024-09-05 23:43:34 +0530

    added PO trav

--- com/tuf/trees/IterativePostorderTravOne.java
+++ com/tuf/trees/IterativePostorderTravOne.java
@@ -47,25 +47,28 @@ public class IterativePostorderTravOne {
     }
 
     public void iterativePostorderTraversalTwo() {
-        if (root == null)
-            return;
-        Stack<Node> st1 = new Stack<>();
-        Stack<Node> st2 = new Stack<>();
-        st1.push(root);
-
-        while (!st1.isEmpty()) {
-            Node topNode = st1.pop();
-            st2.push(topNode);
-            if (topNode.left != null) {
-                st1.push(topNode.left);
+        Node cur = root;
+        Stack<Node> st = new Stack<>();
+        while (cur != null || !st.isEmpty()) {
+            if (cur != null) {
+                st.add(cur);
+                cur = cur.left;
+            } else {
+                Node temp = st.peek().right;
+                if (temp == null) {
+                    temp = st.peek();
+                    st.pop();
+                    System.out.print(temp.value + " ");
+                    while (!st.empty() && temp == st.peek().right) {
+                        temp = st.peek();
+                        st.pop();
+                        System.out.print(temp.value + " ");
+                    }
+                } else {
+                    cur = temp;
+                }
             }
-            if (topNode.right != null)
-                st1.push(topNode.right);
-        }
 
-        while (!st2.isEmpty()) {
-            Node topNode = st2.pop();
-            System.out.print(topNode.value + " ");
         }
     }
 
@@ -79,7 +82,7 @@ public class IterativePostorderTravOne {
         tree.initiateNodeInsertion(445);
         tree.initiateNodeInsertion(48);
         tree.iterativePostorderTraversal();
-        System.out.println();
+        System.out.println("\n--With Signle stack--");
         tree.iterativePostorderTraversalTwo();
     }
 }
