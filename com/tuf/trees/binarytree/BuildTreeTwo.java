dce082080575983999e88fad8dc107611078df95
kiranpoojary <36165693+kiranpoojary@users.noreply.github.com>
2024-09-14 21:53:03 +0530

    Update BuildTreeTwo.java

--- com/tuf/trees/binarytree/BuildTreeTwo.java
+++ com/tuf/trees/binarytree/BuildTreeTwo.java
@@ -4,12 +4,12 @@ import java.util.HashMap;
 
 public class BuildTreeTwo {
     // Build tree from provided inorder and preoder traversal
-    public static void initiateTreeBuild(int[] preOrder, int[] inOrder) {
+    public static void initiateTreeBuild(int[] postOrder, int[] inOrder) {
         HashMap<Integer, Integer> mp = new HashMap<>();
         for (int i = 0; i < inOrder.length; i++) {
             mp.put(inOrder[i], i);
         }
-        Node root = buildTree(preOrder, 0, preOrder.length - 1, inOrder, 0, inOrder.length - 1, mp);
+        Node root = buildTree(postOrder, 0, postOrder.length - 1, inOrder, 0, inOrder.length - 1, mp);
         InorderTraversal i = new InorderTraversal();
         System.out.println("In-Order");
         i.inorderTraversal(root);
@@ -20,17 +20,17 @@ public class BuildTreeTwo {
 
     }
 
-    public static Node buildTree(int[] preOrder, int preStart, int preEnd, int[] inOrder, int inStart, int inEnd,
+    public static Node buildTree(int[] postOrder, int postStart, int postEnd, int[] inOrder, int inStart, int inEnd,
             HashMap<Integer, Integer> inMapper) {
 
-        if (preStart > preEnd || inStart > inEnd)
+        if (postStart > postEnd || inStart > inEnd)
             return null;
-        Node root = new Node(preOrder[preEnd]);
+        Node root = new Node(postOrder[postEnd]);
         int rootIndexInInOrder = inMapper.get(root.value);
         int numsLeft = rootIndexInInOrder - inStart; // 0 indexing
-        root.left = buildTree(preOrder, preStart, preStart + numsLeft - 1, inOrder, inStart, rootIndexInInOrder - 1,
+        root.left = buildTree(postOrder, postStart, postStart + numsLeft - 1, inOrder, inStart, rootIndexInInOrder - 1,
                 inMapper);
-        root.right = buildTree(preOrder, preStart + numsLeft, preEnd - 1, inOrder, rootIndexInInOrder + 1, inEnd,
+        root.right = buildTree(postOrder, postStart + numsLeft, postEnd - 1, inOrder, rootIndexInInOrder + 1, inEnd,
                 inMapper);
         return root;
     }
