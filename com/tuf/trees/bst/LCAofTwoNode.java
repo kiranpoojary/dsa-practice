10efa0f799133dd591213ac4eff37e1046da77f7
kiranpoojary <36165693+kiranpoojary@users.noreply.github.com>
2024-09-15 17:53:20 +0530

    Create LCAofTwoNode.java

---
+++ com/tuf/trees/bst/LCAofTwoNode.java
@@ -0,0 +1,42 @@
+package com.tuf.trees.bst;
+
+public class LCAofTwoNode {
+    Node root;
+
+    public void createTree() {
+        root = new Node(8);
+
+        root.left = new Node(5);
+        root.left.left = new Node(4);
+        root.left.right = new Node(7);
+        root.left.right.left = new Node(6);
+
+        root.right = new Node(12);
+        root.right.left = new Node(10);
+        root.right.right = new Node(14);
+        root.right.right.left = new Node(13);
+
+    }
+
+    public Node getLCA(Node root, Node node1, Node node2) {
+        if (root == null)
+            return null;
+        if (root.value < node1.value && root.value < node2.value) { // both on right
+            return getLCA(root.right, node1, node2);
+        }
+        if (root.value > node1.value && root.value > node2.value) { // both on left
+            return getLCA(root.left, node1, node2);
+        }
+
+        return root;
+
+    }
+
+    public static void main(String[] args) {
+        LCAofTwoNode tree = new LCAofTwoNode();
+        tree.createTree();
+        Node lcaNode = tree.getLCA(tree.root, tree.root.left.left, tree.root.left.right.left);
+        System.out.println("LCA Node Value    :" + lcaNode.value);
+
+    }
+}
