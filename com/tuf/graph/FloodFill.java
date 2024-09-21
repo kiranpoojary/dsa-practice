cf382341970e35dec6209261a26f3934321b12f3
kiranpoojary <36165693+kiranpoojary@users.noreply.github.com>
2024-09-22 00:07:41 +0530

    Create FloodFill.java

---
+++ com/tuf/graph/FloodFill.java
@@ -0,0 +1,54 @@
+package com.tuf.graph;
+
+public class FloodFill {
+
+    public static void dfsFloodFIll(int[][] image, int[][] imageCopy, int currRow, int currCol, int fillColor) {
+        int[] neighbourRowSetter = { -1, 0, 1, 0 };
+        int[] neighbourColSetter = { 0, 1, 0, -1 };
+        int initialColor = imageCopy[currRow][currCol];
+        imageCopy[currRow][currCol] = fillColor;
+        for (int i = 0; i < 4; i++) {
+            int neighborRow = currRow + neighbourRowSetter[i];
+            int neighborCol = currCol + neighbourColSetter[i];
+            if (neighborRow >= 0 && neighborRow < image.length && neighborCol >= 0 && neighborCol <= image[0].length
+                    && imageCopy[neighborRow][neighborCol] == initialColor) {
+                dfsFloodFIll(image, imageCopy, neighborRow, neighborCol, fillColor);
+            }
+
+        }
+    }
+
+    public static void startFloodFill(int[][] image, int startRow, int startCol, int fillColor) {
+        int[][] imageCopy = image;
+        dfsFloodFIll(image, imageCopy, startRow, startCol, fillColor);
+
+    }
+
+    public static void main(String[] args) {
+        // Example adjacency matrix representing the graph
+        int[][] image = {
+                { 1, 1, 1, 0 },
+                { 2, 2, 0, 0 },
+                { 2, 2, 2, 0 },
+                { 1, 0, 2, 0 },
+                { 1, 1, 2, 0 },
+        };
+        for (int i = 0; i < image.length; i++) {
+            for (int j = 0; j < image[0].length; j++) {
+                System.out.print(image[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        startFloodFill(image, 2, 0, 4);
+        System.out.println();
+
+        for (int i = 0; i < image.length; i++) {
+            for (int j = 0; j < image[0].length; j++) {
+                System.out.print(image[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
