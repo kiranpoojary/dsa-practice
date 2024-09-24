5e309024778ad1d2eab2a6604fcf0cb01b5bc43a
kiranpoojary <36165693+kiranpoojary@users.noreply.github.com>
2024-09-24 22:25:34 +0530

    Update CheckBipartiteGraphDFS.java

--- com/tuf/graph/undirected/CheckBipartiteGraphDFS.java
+++ com/tuf/graph/undirected/CheckBipartiteGraphDFS.java
@@ -2,8 +2,6 @@ package com.tuf.graph.undirected;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class CheckBipartiteGraphDFS {
     // bipatite means : each adj node should painted diff color
@@ -12,15 +10,35 @@ public class CheckBipartiteGraphDFS {
     // intuition: if liner graph it is bipartite
     // intuition: if graph has cycle and cycle has even node its bipartite else not
 
-    public static boolean paintNodeUsingDFS(ArrayList<ArrayList<Integer>> adj, int[] painted, int startNode) {
+    public static boolean paintNodeUsingDFS(ArrayList<ArrayList<Integer>> adj, int[] painted, int startNode,
+            int color) {
+        painted[startNode] = color;
+        ArrayList<Integer> adjChilds = adj.get(startNode);
+        for (Integer node : adjChilds) {
+            if (node == startNode)
+                continue;
+            if (painted[node] == -1) {
+                if (paintNodeUsingDFS(adj, painted, node, 1 - color) == false) // 1-color is 1?0:1;
+                    return false;
+            } else if (painted[node] == color)
+                return false;
+        }
 
         return true;
     }
 
-    public static boolean isBipatite(ArrayList<ArrayList<Integer>> adj) {
+    public static boolean isBipartite(ArrayList<ArrayList<Integer>> adj) {
         int[] painted = new int[adj.size()];
         Arrays.fill(painted, -1);
-        return paintNodeUsingDFS(adj, painted, 1);
+        for (int index = 1; index < adj.size(); index++) {
+            if (painted[index] == -1) {
+                if (paintNodeUsingDFS(adj, painted, index, 0) == false) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
@@ -34,7 +52,7 @@ public class CheckBipartiteGraphDFS {
         adjList.add(new ArrayList<>(Arrays.asList(6, 2, 5)));
         adjList.add(new ArrayList<>(Arrays.asList(7, 4, 8)));
         adjList.add(new ArrayList<>(Arrays.asList(8, 7)));
-        System.out.println("Is bipatite graph-1     :" + isBipatite(adjList));
+        System.out.println("Is bipartite graph-1     :" + isBipartite(adjList));
         // -------------------
         ArrayList<ArrayList<Integer>> adjList2 = new ArrayList<>();
         adjList2.add(new ArrayList<>(Arrays.asList())); // 0 index
@@ -44,7 +62,7 @@ public class CheckBipartiteGraphDFS {
         adjList2.add(new ArrayList<>(Arrays.asList(4, 3, 5, 6)));
         adjList2.add(new ArrayList<>(Arrays.asList(5, 2, 4)));
         adjList2.add(new ArrayList<>(Arrays.asList(6, 4)));
-        System.out.println("Is bipatite graph-2     :" + isBipatite(adjList2));
+        System.out.println("Is bipartite graph-2     :" + isBipartite(adjList2));
     }
 
 }
