0b1259045d26080278b329a853ca21be43a47b8f
kiranpoojary <36165693+kiranpoojary@users.noreply.github.com>
2024-10-17 00:21:13 +0530

    Update MaxSumNotAdj.java

--- com/tuf/dp/MaxSumNotAdj.java
+++ com/tuf/dp/MaxSumNotAdj.java
@@ -28,11 +28,46 @@ public class MaxSumNotAdj {
         return memo[idx];
     }
 
+    public static int maxSumTabulation(int[] arr, int[] memo) {
+        memo[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            int pick = arr[i];
+            if (i > 1)
+                pick += memo[i - 2];
+            int notPick = 0 + memo[i - 1];
+            memo[i] = Math.max(pick, notPick);
+        }
+        return memo[arr.length - 1];
+    }
+
+    public static int maxSumTabulationSpaceOptimized(int[] arr, int[] memo) {
+        int prev = arr[0];
+        int prev2 = 0;
+        memo[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            int pick = arr[i];
+            if (i > 1)
+                pick += prev2;
+            int notPick = 0 + prev;
+            int curi = Math.max(pick, notPick);
+            prev2 = prev;
+            prev = curi;
+        }
+        return prev;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 2, 1, 4, 9 };
         int[] memo = new int[arr.length + 1];
         Arrays.fill(memo, -1);
-        System.out.println("Max non adjecent sum   :" + maxSumRecursive(arr.length - 1, arr));
-        System.out.println("Max non adjecent sum   :" + maxSumMemo(arr.length - 1, arr, memo));
+        System.out.println("Max non adjecent sum recursive          :" + maxSumRecursive(arr.length - 1, arr));
+        System.out.println("Max non adjecent sum meoization         :" + maxSumMemo(arr.length - 1, arr, memo));
+        int[] memo2 = new int[arr.length + 1];
+        Arrays.fill(memo2, -1);
+        System.out.println("Max non adjecent sum tabulation         :" + maxSumTabulation(arr, memo2));
+        int[] memo3 = new int[arr.length + 1];
+        Arrays.fill(memo3, -1);
+        System.out.println("Max non adjecent sum space optimized    :" + maxSumTabulationSpaceOptimized(arr, memo3));
+
     }
 }
